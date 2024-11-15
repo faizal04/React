d07b60e8a3c27b8583ee97a349c73d00c9f8294a
faizal04 <100855720+faizal04@users.noreply.github.com>
2024-11-15 09:52:21 +0530

    far-away

--- far-away/src/components/App.js
+++ far-away/src/components/App.js
@@ -5,8 +5,8 @@ import PackingList from "./PackingList.js";
 import Stats from "./Stats.js";
 
 const initialItems = [
-  { id: 1, description: "Mobile", quantity: 2, packed: false },
-  { id: 2, description: "Charger", quantity: 12, packed: true },
+  // { id: 1, description: "Mobile", quantity: 2, packed: false },
+  // { id: 2, description: "Charger", quantity: 12, packed: true },
 ];
 
 export default function App() {
