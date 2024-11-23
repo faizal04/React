91cb2d5c301a25afb56ebbbf76931c8f0fab88fc
faizal04 <100855720+faizal04@users.noreply.github.com>
2024-11-23 23:14:15 +0530

    averagevalue

--- cinemascope/src/components/WatchedSummary.js
+++ cinemascope/src/components/WatchedSummary.js
@@ -2,9 +2,15 @@ const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 function WactchedSummary({ watched }) {
-  const avgImdbRating = average(watched.map((movies) => movies.imdbRating));
-  const avgUserRating = average(watched.map((movies) => movies.userRating));
-  const avgRuntime = average(watched.map((movies) => movies.runtime));
+  const avgImdbRating = Math.floor(
+    average(watched.map((movies) => movies.imdbRating))
+  );
+  const avgUserRating = Math.floor(
+    average(watched.map((movies) => movies.userRating))
+  );
+  const avgRuntime = Math.floor(
+    average(watched.map((movies) => movies.runtime))
+  );
   return (
     <div className="summary">
       <h2>Movies you watched</h2>
